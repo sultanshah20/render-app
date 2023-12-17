362a5adbe809d42b75c99fbcb717194fe9751fef
sulatnshah20 <sultanshah26893@gmail.com>
2023-12-17 12:25:49 +0530

    create jar file

--- lunch_app/src/main/java/com/example/render_application/lunch_app/Controller/User.java
+++ lunch_app/src/main/java/com/example/render_application/lunch_app/Controller/User.java
@@ -1,14 +1,15 @@
 package com.example.render_application.lunch_app.Controller;
 
+import org.springframework.boot.logging.log4j2.Log4J2LoggingSystem;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class User {
-  
+    
     @GetMapping("/test")
     public String welcome()
-    {
+    {  
         return "welcome in the new world";
     }
 }
